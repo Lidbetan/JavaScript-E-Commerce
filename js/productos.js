962b31d3a66a3b946b02763b5a2dde0e2a0f81a7
lucas id betan <lucas.idbetan@gmail.com>
2023-06-23 22:33:44 -0300

    agregué el evento document.addEventListener(DOMContentLoaded)

--- js/productos.js
+++ js/productos.js
@@ -1,71 +1,73 @@
-function validarCompra(realizar_compra) {
-    if (realizar_compra == "SI" || realizar_compra == "NO") {
-        return true;
-    } else {
-        return false;
-    }
-}/*Esta function sirve para validar la compra, toma como parámetro la variable realizar_compra, la cual se define con un prompt.
-    En caso de que el usuario ingrese "SI" o "NO", retorna true, por el contrario, retorna false*/ 
+document.addEventListener("DOMContentLoaded", () => {
 
-let realizar_compra = "";
-realizar_compra = prompt("Va a realizar alguna compra, por favor responda SI o NO");
+    function validarCompra(realizar_compra) {
+        if (realizar_compra == "SI" || realizar_compra == "NO") {
+            return true;
+        } else {
+            return false;
+        }
+    }/*Esta function sirve para validar la compra, toma como parámetro la variable realizar_compra, la cual se define con un prompt.
+    En caso de que el usuario ingrese "SI" o "NO", retorna true, por el contrario, retorna false*/
 
-let validacion = validarCompra(realizar_compra);
+    let realizar_compra = "";
+    realizar_compra = prompt("Va a realizar alguna compra, por favor responda SI o NO");
 
-while (validacion != true) {
-    realizar_compra = prompt("Respuesta inválida. Por favor, ingrese SI o NO");
-    validacion = validarCompra(realizar_compra);
-} /*Cuando NO ingreso "SI" o "NO", validación es false, en dicho caso me vuelve a pedir que ingrese una respuesta válida*/
+    let validacion = validarCompra(realizar_compra);
 
-if (validacion) { /*La variable validación depende de la funcion validar_compra, cuando esta arroja true, se ejecuta el código siguiente*/ 
+    while (validacion != true) {
+        realizar_compra = prompt("Respuesta inválida. Por favor, ingrese SI o NO");
+        validacion = validarCompra(realizar_compra);
+    } /*Cuando NO ingreso "SI" o "NO", validación es false, en dicho caso me vuelve a pedir que ingrese una respuesta válida*/
 
-    if (realizar_compra == "SI") {
-        let articulo = "";
-        let lista_articulos = "";
-        let total_compra = 0;
+    if (validacion) { /*La variable validación depende de la funcion validar_compra, cuando esta arroja true, se ejecuta el código siguiente*/
 
-        while (articulo != "ESC") {
-            articulo = prompt("Ingrese el código del artículo y/o ESC para finalizar");
-            if (articulo === "ESC") {
-                alert("Artículos comprados: " + "\n" + lista_articulos + " " + "El total de su compra es de: " + "\n" + "$" + total_compra + " Gracias por su compra");
-                break;
-            } else {
-                switch (articulo) {
-                    case "1":
-                        lista_articulos += "Cafe Italiano - $2400" + "\n";
-                        total_compra += 2400;
-                        break;
-                    case "2":
-                        lista_articulos += "Cafe Brasil - $2400" + "\n";
-                        total_compra += 2400;
-                        break;
-                    case "3":
-                        lista_articulos += "Cafe Colombia - $2400" + "\n";
-                        total_compra += 2400;
-                        break;
-                    case "4":
-                        lista_articulos += "Cafetera Filtro - $6500" + "\n";
-                        total_compra += 2400;
-                        break;
-                    case "5":
-                        lista_articulos += "Cafetera Italiana - $9700" + "\n";
-                        total_compra += 2400;
-                        break;
-                    case "6":
-                        lista_articulos += "Jarra Pitcher - $8600" + "\n";
-                        total_compra += 2400;
-                        break;
-                    default:
-                        alert("Ingrese el codigo correcto");
+        if (realizar_compra == "SI") {
+            let articulo = "";
+            let lista_articulos = "";
+            let total_compra = 0;
+
+            while (articulo != "ESC") {
+                articulo = prompt("Ingrese el código del artículo y/o ESC para finalizar");
+                if (articulo === "ESC") {
+                    alert("Artículos comprados: " + "\n" + lista_articulos + " " + "El total de su compra es de: " + "\n" + "$" + total_compra + " Gracias por su compra");
+                    break;
+                } else {
+                    switch (articulo) {
+                        case "1":
+                            lista_articulos += "Cafe Italiano - $2400" + "\n";
+                            total_compra += 2400;
+                            break;
+                        case "2":
+                            lista_articulos += "Cafe Brasil - $2400" + "\n";
+                            total_compra += 2400;
+                            break;
+                        case "3":
+                            lista_articulos += "Cafe Colombia - $2400" + "\n";
+                            total_compra += 2400;
+                            break;
+                        case "4":
+                            lista_articulos += "Cafetera Filtro - $6500" + "\n";
+                            total_compra += 2400;
+                            break;
+                        case "5":
+                            lista_articulos += "Cafetera Italiana - $9700" + "\n";
+                            total_compra += 2400;
+                            break;
+                        case "6":
+                            lista_articulos += "Jarra Pitcher - $8600" + "\n";
+                            total_compra += 2400;
+                            break;
+                        default:
+                            alert("Ingrese el codigo correcto");
+                    }
                 }
             }
+        } else if (realizar_compra == "NO") {//Si la respuesta ingresada por el usuario inicialmente fuera "NO", arroja el siguiente mensaje.
+            alert("Que tenga buen día");
         }
-    } else if (realizar_compra == "NO") {//Si la respuesta ingresada por el usuario inicialmente fuera "NO", arroja el siguiente mensaje.
-        alert("Que tenga buen día");
+
     }
-
-} 
-
+});
 
 
 
