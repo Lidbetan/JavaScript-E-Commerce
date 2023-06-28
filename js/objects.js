28ec5d4f6333e31f943734c242ea1161596c5263
lucas id betan <lucas.idbetan@gmail.com>
2023-06-28 19:31:09 -0300

    Nuevos archivos .js, voy a empezar  a agregar arrays y objects

---
+++ js/objects.js
@@ -0,0 +1,10 @@
+const listaProductos = [
+    { nombre: "Café italiano", precio: 2400 },
+    { nombre: "Café brasil", precio: 2400 },
+    { nombre: "Café colombia", precio: 2400 },
+    { nombre: "Cafetera filtro", precio: 6500 },
+    { nombre: "Cafetera italiana", precio: 9700 },
+    { nombre: "Jarra pitcher", precio: 8600 },
+]
+
+console.table(listaProductos);
